c586c6d082bfdc589842ac46978fe72855676b94
Baris Basar <barisbasar@cotrai.com>
2026-02-14 19:10:51 +0100

    Fix test import path

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,11 @@
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent / "src"))
+
 from fastapi.testclient import TestClient
 
-from src.main import app
+from main import app
 
 client = TestClient(app)
 
